3e585f1805380fb6dd2c4c6ba55d4be1ec55d7bf
Aman kohare <amankohare@gmail.com>
2024-12-23 14:25:40 +0530

    add style

---
+++ src/app/blogs/BlogData.tsx
@@ -0,0 +1,62 @@
+const BlogData = [
+  {
+    description: "Welcome to the next chapter in web development with Next.js! With every iteration...",
+    title: "Introducing Next.js: Unveiling Exciting New Features",
+    src: "/blogs/next.png",
+    ctaText: "Visit",
+    ctaLink: "https://medium.com/@amankohare/introducing-next-js-unveiling-exciting-new-features-374bb4830b83",
+  },
+  {
+    description: "list of 10 tools that developers across various domains and technologies may find useful..",
+    title: "10 tools that developers across various domains and technologies may find useful",
+    src: "/blogs/tools.jpeg",
+    ctaText: "Visit",
+    ctaLink: "https://medium.com/@amankohare/10-tools-that-developers-across-various-domains-and-technologies-may-find-useful-05c52d5699ee",
+  },
+
+  {
+    description: "Vercel is a cloud platform that provides a serverless deployment platform for frontend developers...",
+    title: "How to deploy website on vercel?",
+    src: "/blogs/vercel.jpg",
+    ctaText: "Visit",
+    ctaLink: "https://medium.com/@amankohare/how-to-deploy-website-on-vercel-da6c54a184d6",
+  },
+  {
+    description: "Becoming a successful web developer involves a combination of technical skills, continuous learning...",
+    title: "How To Become Successful Web developer",
+    src: "/blogs/developer.webp",
+    ctaText: "Visit",
+    ctaLink: "https://medium.com/@amankohare/how-to-become-successful-web-developer-f44874874e23",
+  },
+  {
+    description: "Welcome to the next chapter in web development with Next.js! With every iteration...",
+    title: "Introducing Next.js: Unveiling Exciting New Features",
+    src: "/blogs/next.png",
+    ctaText: "Visit",
+    ctaLink: "https://medium.com/@amankohare/introducing-next-js-unveiling-exciting-new-features-374bb4830b83",
+  },
+  {
+    description: "list of 10 tools that developers across various domains and technologies may find useful..",
+    title: "10 tools that developers across various domains and technologies may find useful",
+    src: "/blogs/tools.jpeg",
+    ctaText: "Visit",
+    ctaLink: "https://medium.com/@amankohare/10-tools-that-developers-across-various-domains-and-technologies-may-find-useful-05c52d5699ee",
+  },
+
+  {
+    description: "Vercel is a cloud platform that provides a serverless deployment platform for frontend developers...",
+    title: "How to deploy website on vercel?",
+    src: "/blogs/vercel.jpg",
+    ctaText: "Visit",
+    ctaLink: "https://medium.com/@amankohare/how-to-deploy-website-on-vercel-da6c54a184d6",
+  },
+  {
+    description: "Becoming a successful web developer involves a combination of technical skills, continuous learning...",
+    title: "How To Become Successful Web developer",
+    src: "/blogs/developer.webp",
+    ctaText: "Visit",
+    ctaLink: "https://medium.com/@amankohare/how-to-become-successful-web-developer-f44874874e23",
+  },
+];
+
+export default BlogData
--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import BlogData from "./BlogData";
+import Link from "next/link";
 
-const page = () => {
+const Page = () => {
   return (
-    <div>Comming soon</div>
-  )
-}
+    <div className="min-h-screen bg-background px-8 py-12 md:px-24 lg:py-18">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {BlogData.map((blog, index) => (
+          <Link href={blog.ctaLink} target="__blank">
+            <Card
+              key={index}
+              className="flex flex-col h-full overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300"
+            >
 
-export default page
+              <div className="relative h-48">
+                <img
+                  src={blog.src}
+                  alt={blog.title}
+                  className="w-full h-full object-cover"
+                />
+              </div>
+              <CardHeader className="p-4">
+                <CardTitle className="text-xl font-semibold mb-2">
+                  {blog.title}
+                </CardTitle>
+                <span className="text-sm text-muted-foreground">
+                  {blog.description}
+                </span>
+              </CardHeader>
+            </Card>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Page;
